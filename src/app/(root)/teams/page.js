f7672ad5f36acaf1a116f769b269d52af8a846bb
Kartikay <sky.kartikay@gmail>
2023-12-22 19:17:56 +0530

    trial003

--- src/app/(root)/teams/page.js
+++ src/app/(root)/teams/page.js
@@ -1,34 +1,34 @@
 // pages/teams/index.js
-"use client"
-import React, { useState } from 'react';
+// "use client"
+import React from 'react';
 import TeamSlider from '../../../components/TeamSlider';
-import RootLayout from '../layout.js';
-import { useRouter } from 'next/navigation';
+// import RootLayout from '../layout.js';
+// import { useRouter } from 'next/navigation';
 
-const teams = [
-  { name: 'Tech', slug: 'tech' },
-  { name: 'Core', slug: 'core' },
-  { name: 'Event Management', slug: 'event-management' },
-  { name: 'PR', slug: 'pr' },
-  { name: 'Sponsorship', slug: 'sponsorship' },
-  { name: 'Content', slug: 'content' },
-  { name: 'Logistics', slug: 'logistics' },
-  { name: 'Leads', slug: 'leads' },
-];
+// const teams = [
+//   { name: 'Tech', slug: 'tech' },
+//   { name: 'Core', slug: 'core' },
+//   { name: 'Event Management', slug: 'event-management' },
+//   { name: 'PR', slug: 'pr' },
+//   { name: 'Sponsorship', slug: 'sponsorship' },
+//   { name: 'Content', slug: 'content' },
+//   { name: 'Logistics', slug: 'logistics' },
+//   { name: 'Leads', slug: 'leads' },
+// ];
 
 const Page = () => {
-    const [selectedTeam, setSelectedTeam] = useState(null);
-    const router = useRouter();
+//     const [selectedTeam, setSelectedTeam] = useState(null);
+//     const router = useRouter();
   
-    const handleTeamClick = (team) => {
-      setSelectedTeam(team);
-      router.push(`/teams/${team.slug}`);
-    };  
+//     const handleTeamClick = (team) => {
+//       setSelectedTeam(team);
+//       router.push(`/teams/${team.slug}`);
+//     };  
 
   return (
       <div className="mx-4 my-8">
         <h2 className="text-2xl font-bold mb-4">Teams</h2>
-        <TeamSlider teams={teams} handleTeamClick={handleTeamClick} selectedTeam={selectedTeam} />
+        {/* <TeamSlider teams={teams} handleTeamClick={handleTeamClick} selectedTeam={selectedTeam} /> */}
       </div>
   );
 };
