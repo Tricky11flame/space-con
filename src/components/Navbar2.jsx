26ab1a5c0ccc57f3dd5e3b05c8924bde254c0f6c
Asvi <asverarise@gmail.com>
2024-01-06 21:26:22 +0530

    event register and event page update

--- src/components/Navbar2.jsx
+++ src/components/Navbar2.jsx
@@ -29,9 +29,9 @@ const Navbar2 = () => {
 
         {/* Menu Items (Desktop) */}
         <div className="hidden lg:flex space-x-6 text-lg ml-16">
-            <a href="events" className="nav4 transition-all duration-300 hover:scale-110">EVENTS</a>
-            <a href="schedule" className="nav4 transition-all duration-300 hover:scale-110">SCHEDULE</a>
-            <a href="about" className="nav4 transition-all duration-300 hover:scale-110">ABOUT US</a>
+            <Link href="/events" className="nav4 transition-all duration-300 hover:scale-110">EVENTS</Link>
+            <Link href="/schedule" className="nav4 transition-all duration-300 hover:scale-110">SCHEDULE</Link>
+            <Link href="/about" className="nav4 transition-all duration-300 hover:scale-110">ABOUT US</Link>
             <Link className="nav4 transition-all duration-300 hover:scale-110" href="/signup"><button>SignUp</button></Link>
             <Link className="nav4 transition-all duration-300 hover:scale-110" href="/login"><button>LogIn</button></Link>
         </div>
@@ -46,9 +46,9 @@ const Navbar2 = () => {
         id="mobile-menu"
         className={`lg:hidden ${isMobileMenuVisible ? '' : 'hidden'} bg-gray-700/40 text-center w-[100%] mt-[80px] fixed `}
       >
-        <a href="events" className="block py-2 px-4 nav-link hover:bg-purple-700 border-b-2">EVENTS</a>
-        <a href="schedule" className="block py-2 px-4 nav-link hover:bg-purple-700 border-b-2">SCHEDULE</a>
-        <a href="about" className="block py-2 px-4 nav-link hover:bg-purple-700 border-b-2">ABOUT US</a>
+        <Link href="/events" className="block py-2 px-4 nav-link hover:bg-purple-700 border-b-2">EVENTS</Link>
+        <Link href="/schedule" className="block py-2 px-4 nav-link hover:bg-purple-700 border-b-2">SCHEDULE</Link>
+        <Link href="/about" className="block py-2 px-4 nav-link hover:bg-purple-700 border-b-2">ABOUT US</Link>
         <div className="flex justify-center p-2 gap-16">
           <button className="btn hover:bg-transparent bg-spaceCon-tertiary px-2 rounded-xl uppercase shadow-3xl">SignUp</button>
           <button className="btn hover:bg-transparent bg-spaceCon-tertiary px-2 rounded-xl uppercase shadow-3xl">Login</button>
